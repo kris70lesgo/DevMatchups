57358c6086fa6a3df34852c76172f9f8ee4abbd0
Agastya Khati <krishkhati70@gmail.com>
2025-08-06 12:26:54 +0530

    h

--- app/(dashboard)/(routes)/teams/page.tsx
+++ app/(dashboard)/(routes)/teams/page.tsx
@@ -2,15 +2,27 @@ import React from 'react'
 import PostsTab from './components/PostsTab';
 import {FilterTab} from './components/FilterTab';
 
-
 const page = () => {
   return (
-    <div className='min-h-screen text-black dark:bg-background bg-background dark:text-white  '>
-      
-      <div className='grid grid-cols-1 lg:grid-cols-4 gap-2 w-full mt-10 pr-5  '>
-        <FilterTab />
-        <div className='col-span-3 px-5 lg:px-0 pt-4'>
-        <PostsTab />
+    <div className='min-h-screen text-black dark:bg-background bg-background dark:text-white'>
+      {/* Main container with proper constraints */}
+      <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-6 pb-12'>
+        
+        {/* Grid layout with improved spacing */}
+        <div className='grid grid-cols-1 lg:grid-cols-4 gap-6 lg:gap-8 w-full'>
+          
+          {/* Filter sidebar */}
+          <div className='lg:col-span-1'>
+            <div className='sticky top-6'>
+              <FilterTab />
+            </div>
+          </div>
+          
+          {/* Main content area */}
+          <div className='lg:col-span-3'>
+            <PostsTab />
+          </div>
+          
         </div>
       </div>      
     </div>
@@ -18,3 +30,4 @@ const page = () => {
 }
 
 export default page
+
